de298b146356fc2c2a89398e19aa936829e4d17f
tarik619 <itarik707@gmail.com>
2024-01-06 23:13:49 +0600

    creating file uploadng functionality with multer

---
+++ src/middlewares/multer.ts
@@ -0,0 +1,12 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, "uploads");
+  },
+  filename(req, file, callback) {
+    callback(null, file.originalname);
+  },
+});
+
+export const signleUpload = multer({ storage }).single("photo");
