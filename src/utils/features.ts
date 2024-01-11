af52925e8848a120480613becb8c3c1c29e4dac7
tarik619 <itarik707@gmail.com>
2024-01-11 11:48:11 +0600

    finishing getdashboardstats api else part and adding calculatepercent feature in features file

--- src/utils/features.ts
+++ src/utils/features.ts
@@ -60,3 +60,9 @@ export const reduceStock = async (orderItems: OrerItemType[]) => {
     await product.save();
   }
 };
+
+export const calculatePercentage = (thisMonth: number, lastMonth: number) => {
+  if (lastMonth === 0) return thisMonth * 100;
+  const percent = ((thisMonth - lastMonth) / lastMonth) * 100;
+  return Number(percent.toFixed(0));
+};
