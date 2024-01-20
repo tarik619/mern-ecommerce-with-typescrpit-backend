1b90cc702214226b072a8391de89b0f28835f0f4
tarik619 <itarik707@gmail.com>
2024-01-20 11:25:45 +0600

    small changes in payment route

--- dist/controllers/payment.controller.js
+++ dist/controllers/payment.controller.js
@@ -3,43 +3,41 @@ import { TryCatch } from "../middlewares/error.js";
 import { Coupon } from "../models/coupon.model.js";
 import errorHandler from "../utils/utility-class.js";
 export const createPaymentIntent = TryCatch(async (req, res, next) => {
-    const { amount } = req.body;
-    if (!amount)
-        return next(new errorHandler("please enter amount", 400));
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: Number(amount) * 120,
-        currency: "usd",
-    });
-    return res
-        .status(201)
-        .json({ success: true, clientSecret: paymentIntent.client_secret });
+  const { amount } = req.body;
+  if (!amount) return next(new errorHandler("please enter amount", 400));
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: Number(amount) * 120,
+    currency: "usd",
+  });
+  return res
+    .status(201)
+    .json({ success: true, clientSecret: paymentIntent.client_secret });
 });
 export const newCoupon = TryCatch(async (req, res, next) => {
-    const { coupon, amount } = req.body;
-    if (!coupon || !amount)
-        return next(new errorHandler("please enter both coupon and amount", 400));
-    await Coupon.create({ code: coupon, amount });
-    return res
-        .status(201)
-        .json({ success: true, message: `Coupon ${coupon}created successfully` });
+  const { coupon, amount } = req.body;
+  if (!coupon || !amount)
+    return next(new errorHandler("please enter both coupon and amount", 400));
+  await Coupon.create({ code: coupon, amount });
+  return res.status(201).json({
+    success: true,
+    message: `Coupon ${coupon} created successfully`,
+  });
 });
 export const applyDiscount = TryCatch(async (req, res, next) => {
-    const { coupon } = req.query;
-    const discount = await Coupon.findOne({ code: coupon });
-    if (!discount)
-        return next(new errorHandler("coupon not found", 404));
-    return res.status(201).json({ success: true, discount: discount.amount });
+  const { coupon } = req.query;
+  const discount = await Coupon.findOne({ code: coupon });
+  if (!discount) return next(new errorHandler("coupon not found", 404));
+  return res.status(201).json({ success: true, discount: discount.amount });
 });
 export const allCoupons = TryCatch(async (req, res, next) => {
-    const coupons = await Coupon.find({});
-    return res.status(201).json({ success: true, coupons });
+  const coupons = await Coupon.find({});
+  return res.status(201).json({ success: true, coupons });
 });
 export const deleteCoupons = TryCatch(async (req, res, next) => {
-    const { id } = req.params;
-    const coupon = await Coupon.findByIdAndDelete(id);
-    if (!coupon)
-        return next(new errorHandler("invalid coupon id", 400));
-    return res
-        .status(201)
-        .json({ success: true, message: `coupon  deleted successfully` });
+  const { id } = req.params;
+  const coupon = await Coupon.findByIdAndDelete(id);
+  if (!coupon) return next(new errorHandler("invalid coupon id", 400));
+  return res
+    .status(201)
+    .json({ success: true, message: `coupon  deleted successfully` });
 });
--- dist/routes/payment.route.js
+++ dist/routes/payment.route.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.js";
-import { allCoupons, applyDiscount, createPaymentIntent, deleteCoupons, newCoupon, } from "../controllers/payment.controller.js";
+import {
+  allCoupons,
+  applyDiscount,
+  createPaymentIntent,
+  deleteCoupons,
+  newCoupon,
+} from "../controllers/payment.controller.js";
 const router = express.Router();
 router.post("/create", createPaymentIntent);
 router.post("/coupon/new", newCoupon);
-router.get("/discount", adminOnly, applyDiscount);
+router.get("/discount", applyDiscount);
 router.get("/coupon/all", adminOnly, allCoupons);
 router.delete("/coupon/:id", adminOnly, deleteCoupons);
 export default router;
